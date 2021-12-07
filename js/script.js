6063111c01e0b356d0bbd2a91d23579fccf8516b
rexhsiao <f9281056@gmail.com>
2021-12-07 11:00:49 +1000

    Work dropdown menu

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 function workSelection(value){
     $('.card').show();
+    $('#workMobileTitle').text('All');
     if(value == 2){
         $('.card').not('.spatial').hide();
+        $('#workMobileTitle').text('Spatial Designs');
     }
     if(value == 3){
         $('.card').not('.compositeCreation').hide();
+        $('#workMobileTitle').text('Composite Creations');
     }
     if(value == 4){
         $('.card').not('.fineArt').hide();
+        $('#workMobileTitle').text('Visual Arts');
     }
     changeActive(value);
 }
